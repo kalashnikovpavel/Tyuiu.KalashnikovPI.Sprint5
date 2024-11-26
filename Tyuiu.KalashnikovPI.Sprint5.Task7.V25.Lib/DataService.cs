8a13dca1b27995bad4ffb7e6499e6cb1c117c5b8
kalashnikovpi <kalashnicovp@yandex.ru>
2024-11-26 19:54:52 +0500

    Task7

---
+++ Tyuiu.KalashnikovPI.Sprint5.Task7.V25.Lib/DataService.cs
@@ -0,0 +1,41 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.KalashnikovPI.Sprint5.Task7.V25.Lib
+{
+    public class DataService : ISprint5Task7V25
+    {
+        public string LoadDataAndSave(string path)
+        {
+            char[] eng = new char[] { 'H', 'e', 'l', 'o', 'I', 's', 'M', 'y', 'f', 'i', 'r', 't', 'P', 'r', 'o', 'g', 'a', 'm' };
+            string pathSaveFile = Path.Combine(new string[] { Path.GetTempPath(), "OutPutDataFileTask7V25.txt" });
+            FileInfo fileInfo = new FileInfo(pathSaveFile);
+            bool fileExists = fileInfo.Exists;
+
+            if (fileExists)
+            {
+                File.Delete(pathSaveFile);
+            }
+
+            string strLine = "";
+            using (StreamReader reader = new StreamReader(path))
+            using (StreamWriter writer = new StreamWriter(pathSaveFile))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] words = line.Split(new char[] { ' ', ',', '.', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+                    foreach(string word in words)
+                    {
+                        if (!word.Any(c => eng.Contains(c)))
+                        {
+                            
+                            writer.Write(word + " ");
+                        }
+                    }
+                    writer.WriteLine();
+                }
+            }
+            return pathSaveFile;
+            
+        }
+    }
+}
