a9b89a136a7711de9b1fe3605a742af1d3d1f0f8
kalashnikovpi <kalashnicovp@yandex.ru>
2024-11-26 20:16:54 +0500

    Task7

--- Tyuiu.KalashnikovPI.Sprint5.Task7.V25.Lib/DataService.cs
+++ Tyuiu.KalashnikovPI.Sprint5.Task7.V25.Lib/DataService.cs
@@ -1,41 +1,46 @@
-﻿using tyuiu.cources.programming.interfaces.Sprint5;
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
 namespace Tyuiu.KalashnikovPI.Sprint5.Task7.V25.Lib
 {
     public class DataService : ISprint5Task7V25
     {
         public string LoadDataAndSave(string path)
         {
-            char[] eng = new char[] { 'H', 'e', 'l', 'o', 'I', 's', 'M', 'y', 'f', 'i', 'r', 't', 'P', 'r', 'o', 'g', 'a', 'm' };
-            string pathSaveFile = Path.Combine(new string[] { Path.GetTempPath(), "OutPutDataFileTask7V25.txt" });
-            FileInfo fileInfo = new FileInfo(pathSaveFile);
-            bool fileExists = fileInfo.Exists;
+            char[] eng = new char[] { 'H', 'e', 'l', 'o', 'I', 's', 'M', 'y', 'F', 'i', 'r', 't', 'P', 'r', 'o', 'g', 'a', 'm' };
+            string pathSaveFile = Path.Combine(Path.GetTempPath(), "OutPutDataFileTask7V25.txt");
 
-            if (fileExists)
+           
+            if (File.Exists(pathSaveFile))
             {
                 File.Delete(pathSaveFile);
             }
 
-            string strLine = "";
             using (StreamReader reader = new StreamReader(path))
-            using (StreamWriter writer = new StreamWriter(pathSaveFile))
             {
                 string line;
                 while ((line = reader.ReadLine()) != null)
                 {
-                    string[] words = line.Split(new char[] { ' ', ',', '.', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
-                    foreach(string word in words)
+                    StringBuilder strLine = new StringBuilder();
+
+                    foreach (char c in line)
                     {
-                        if (!word.Any(c => eng.Contains(c)))
+                        
+                        if (!eng.Contains(c))
                         {
-                            
-                            writer.Write(word + " ");
+                            strLine.Append(c);
                         }
                     }
-                    writer.WriteLine();
+
+                    
+                    File.AppendAllText(pathSaveFile, strLine.ToString().Trim() + Environment.NewLine);
                 }
             }
+
             return pathSaveFile;
-            
         }
     }
 }
